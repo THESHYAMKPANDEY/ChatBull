3d1a2defd56d0ce6a70d6c1d9c3b349b467934fc
THESHYAMKPANDEY <theshyamkpandey@gmail.com>
2026-01-23 19:53:11 +0530

    fix: resolve react-async-hook build error for web

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,6 +3,7 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const config = getSentryExpoConfig(__dirname);
 
 config.resolver.assetExts = [...config.resolver.assetExts, 'bin'];
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
 config.transformer.unstable_allowRequireContext = true;
 
 module.exports = config;
