bec9363ffb9f24a7682c674af8ab16b017605e01
THESHYAMKPANDEY <theshyamkpandey@gmail.com>
2026-02-02 23:45:53 +0530

    Use GOOGLE_SERVICES_JSON only when set

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -3,13 +3,13 @@ const base = require("./app.json");
 module.exports = () => {
   const expo = base.expo || {};
   const android = expo.android || {};
+  const googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
 
   return {
     ...expo,
     android: {
       ...android,
-      googleServicesFile:
-        process.env.GOOGLE_SERVICES_JSON || android.googleServicesFile || "./google-services.json",
+      ...(googleServicesFile ? { googleServicesFile } : {}),
     },
   };
 };
