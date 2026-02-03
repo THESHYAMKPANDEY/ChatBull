5790c616f788cf8988fd5d8f835dc4c9a72f9c6b
THESHYAMKPANDEY <theshyamkpandey@gmail.com>
2026-02-03 20:53:22 +0530

    Inject EXPO_PUBLIC env into expo extra for web

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,8 +5,26 @@ module.exports = () => {
   const android = expo.android || {};
   const googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
 
+  // Inject EXPO_PUBLIC_* at build time for web/SSG
+  const publicEnv = {
+    EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    EXPO_PUBLIC_FIREBASE_PROJECT_ID: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    EXPO_PUBLIC_API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+    EXPO_PUBLIC_SOCKET_URL: process.env.EXPO_PUBLIC_SOCKET_URL,
+  };
+
+  const extra = {
+    ...(expo.extra || {}),
+    ...Object.fromEntries(Object.entries(publicEnv).filter(([, v]) => v)),
+  };
+
   return {
     ...expo,
+    extra,
     android: {
       ...android,
       ...(googleServicesFile ? { googleServicesFile } : {}),
